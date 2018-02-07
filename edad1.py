ac1c4b7e4806f52001db46593394c9390e013c6c
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-02-07 23:45:23 +0100

    Add files via upload

---
+++ edad1.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+# Daniel Rodas
+# Fecha de creación 07/02/2018
+
+edad=input("¿Que edad tiene usted?  ")
+
+if edad >= 15 and edad <= 17 :
+	
+	print ( "Usted solo puede entrar a las sesiones de las tardes " )
+else: 
+	
+	print ( "No puede pasar" )
