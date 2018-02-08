477ec4f5bc07af84c8195ff096953b98d359b242
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-02-08 12:03:18 +0100

    Add files via upload

---
+++ pythonclase1.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+# Daniel Rodas
+# Fecha de creación: 08/02/2018
+edad = input("Escriba su edad :  ")
+
+if ( edad >= 18 and edad <= 23 ) or ( edad == 17 ) :
+	
+	print ( "Usted puede entrar en la sesión juvenil" )
