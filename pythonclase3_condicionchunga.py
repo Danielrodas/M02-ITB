d2672b78e03fdc337c7b749b537c8c42fe7d0c36
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-02-08 12:03:45 +0100

    Add files via upload

---
+++ pythonclase3_condicionchunga.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+# Daniel Rodas
+# Fecha de creación: 08/02/2018
+numero = input ( "Introduzca su edad :  " )
+
+if ( numero % 2 == 0 ) or ( numero >= -10 and numero <= 40 ) or ( numero < 0 ) :
+	
+	print numero , " cumple perfectamente la condición"
