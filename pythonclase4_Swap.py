712d74300b45269f4f8612c8de745fecc7dd4cba
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-02-08 12:15:19 +0100

    Add files via upload

---
+++ pythonclase4_Swap.py
@@ -0,0 +1,40 @@
+# encoding: utf-8
+# Daniel Rodas
+# Fecha de creación: 08/02/2018
+import time
+
+mano_der = "Movil"
+
+mano_iz = "temporal"
+
+mano_extra = " :o "
+
+
+
+print mano_der
+
+print mano_iz
+
+
+time.sleep(1)
+
+print mano_extra == mano_der
+
+
+time.sleep (1)
+
+print mano_der == mano_iz
+
+
+time.sleep (1)
+
+print mano_iz == mano_extra
+
+
+
+
+times.sleep(2)
+
+print mano_der
+print mano_iz
+print mano_extra
