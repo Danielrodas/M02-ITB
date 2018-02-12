2cd97f800c6625acb236e695c230308bfcde5432
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-02-12 20:12:32 +0100

    Update tres_numeros.py

--- tres_numeros.py
+++ tres_numeros.py
@@ -14,18 +14,21 @@ time.sleep (1)
 
 C = input ( "Introduzca un número diferente: " )
 
-
+#Primeramente compruebo que no haya números iguales
 if (( A == B  ) and ( A == C )) or (( B == A ) and ( B ==  C )) or (( C == A ) and ( C == B )) :
 	print  "Todos los números son iguales" 
 
 else:
-				
+    
+    # Ahora compruebo que haya dos números iguales
 	if ( ( (A == B) and not ( A == C ) ) or ( ( B == C ) and not ( B == A ) ) or ( ( A == C ) and not ( A == B) ) ):
 					
 		print "hay dos números iguales"  
 		
 	else:
 		
+		# Una vez comprovado lo anterior procedemos a comprobar que número es mayor y cúal es menor 
+		# El mismo comentario de arriba se procederá a todas las siguientes comparaciones que vienen a continuación
 		if ( ( A > B ) and ( A > C ) ) :
 	
 			print  "El número " , A , " es el mayor" 
