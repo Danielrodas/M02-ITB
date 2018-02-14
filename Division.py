5747d88e361baf41d02bff5b4e7c0ddd482a7a78
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-02-14 19:17:05 +0100

    Update Division.py

--- Division.py
+++ Division.py
@@ -1,24 +1,24 @@
 #encoding: UTF-8 
 
 # Daniel A Rodas
-# Fecha de creación:  12/02/2018
+# Fecha de creación:  12/02/2018  Fecha de modificación: 14/02/2018 
 import time
 
-# En las dos siguientes lineas creo dos variables A y B las cuales su valor será asignado por el usuario
-A = input( "Introduzca un número dividendo :  " )
-B = input( "Introduzca un número divisor :    " )
+# En las dos siguientes lineas creo dos variables Dividendo y Divisor las cuales su valor será asignado por el usuario
+Dividendo = input( "Introduzca un número dividendo :  " 
+Dividor   = input( "Introduzca un número divisor :    " )
 
 # En la siguiente linea creo una variable C que cuyo valor será el resultado de la división de A y B
-C =  A / B  
+Cociente =  Dividendo / Divisor
 
 # En la siguiente linea creo una variable que nos dará el modulo de la división entre A y B
-D = A%B
+Resto = Dividendo % Dividor
+ 
 
-
-if  ( A % B == 0 ) :
+if  ( Resto == 0 ) :
 	
-	print "La división es exacta. Conciente: ", C
+	print "La división es exacta. Conciente: ", Conciente
 	
 else:
 	
-	print "La división no es exacta. Conciente: ", C , "  Resto: ", D 
+	print "La división no es exacta. Conciente: ", Cociente , "  Resto: ", Resto
