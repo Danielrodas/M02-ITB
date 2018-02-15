0094318b7ee5496c88a955f8676763000928e052
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-02-15 22:41:06 +0100

    Update pythonclase2.py

--- pythonclase2.py
+++ pythonclase2.py
@@ -1,8 +1,15 @@
 # encoding: utf-8
 # Daniel Rodas
 # Fecha de creación: 08/02/2018
+# Fecha de creación: 08/02/2018  
+# Modificación: 15/02/2018 ========= Nada mas agregé el print del else y compuse bien el print del if.
 
 numero = input ( "Introduzca un numero :  " )
 
 if ( numero % 2 == 0 ) :
-	print ( numero )
+
+	print  "Qué bonito número par."
+ 
+else:
+	
+	print "Qué número más vulgar"
