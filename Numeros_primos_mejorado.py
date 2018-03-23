62a91ef3a228aa487cef5b1f88521c89dbbc5efa
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-03-23 01:28:37 +0100

    Add files via upload

--- Numeros_primos_mejorado.py
+++ Numeros_primos_mejorado.py
@@ -12,24 +12,26 @@ contar = limite
 aciertos = 0
 
 
-if (limite <1) :
-	print ""
-while ( salir=="N" ):
-    # Hago cosas
-    if (contar < limite and contar > 1):
-        if (limite % contar == 0):
-            aciertos = aciertos +1        
-    # Incremento
-
-    contar = contar - 1
-    
-
-    # Activo indicador de salida si toca
-    if ( contar < 1): # Condición de salida
-        salir = "S"
-        
-if (aciertos > 0):
-    print "\n  No es número primo"
-
+if (limite < 1):
+    print ""
+    salir = "S"
 else:
-    print "\n Si es número primo"
+    while ( salir=="N" ):
+        # Hago cosas
+        if (contar < limite and contar > 1):
+            if (limite % contar == 0):
+                aciertos = aciertos +1        
+        # Incremento
+
+        contar = contar - 1
+        
+
+        # Activo indicador de salida si toca
+        if ( contar < 1): # Condición de salida
+            salir = "S"
+            
+    if (aciertos > 0):
+        print "\nNo es número primo\n"
+
+    else:
+        print "\nSi es número primo\n"
