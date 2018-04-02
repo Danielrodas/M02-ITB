8157e0160a640787a0b4f0bb6c973a27870b88c6
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-04-02 22:31:18 +0200

    Add files via upload

---
+++ ejercicio fibonacci.py
@@ -0,0 +1,52 @@
+# encoding: UTF-8
+# Daniel A. Rodas Flores
+import os    #en esta linea estoy importando "os" para poder utilizar "clear"
+
+
+# Inicializaciones
+os.system("clear")
+print "\n"
+
+####################################
+actual = 1
+siguiente = 1
+extra = 0 
+###############################
+vuelta = 2
+limite = input("Introduzca el número de vueltas:  ")
+#################################
+salir = "N"
+if (limite < 0):
+    salir = "S"
+
+elif (limite == 0):
+    print actual
+    print siguiente
+    salir = "S"
+
+elif (limite == 1):
+    actual = 1
+    siguiente = 1
+    print actual
+    print siguiente
+    salir = "S"
+
+elif ( limite > 1 ): 
+
+    extra = actual + siguiente
+    print actual 
+    print siguiente  
+    while ( salir=="N" ):
+        # Hago cosas
+        print extra
+        
+        # Incremento
+        actual = siguiente
+        siguiente = extra 
+        extra = actual + siguiente
+        
+        
+        vuelta = vuelta + 1
+        # Activo indicador de salida si toca
+        if (vuelta > limite): # Condición de salida
+                salir = "S"
