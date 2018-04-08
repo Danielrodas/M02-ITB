5e2b9e20523b3515ae8adf637f5ab018f076008d
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-04-08 18:59:26 +0200

    Update pythonclase4_Swap.py

--- pythonclase4_Swap.py
+++ pythonclase4_Swap.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 # Daniel Rodas
 # Fecha de creación: 08/02/2018
+# Fecha de modificación: 08/04/2018
 
 mano_der = "Movil"
 
@@ -8,18 +9,36 @@ mano_iz = "temporal"
 
 mano_extra = ""
 
+print "\n"
+print "######## ANTES #########"
+print "\n"
 print mano_der , ", der" 
 print mano_iz , ", iz"
-print mano_extra, ", ex"
+print "\n"
 
+print "#### PRIMERA PARTE PROCESO ####"
 mano_extra = mano_der
+print "\n"
 print mano_extra, ", ex" 
 print mano_der, ", der"
+print "\n"
 
+print "#### SEGUNDA PARTE PROCESO ####"
+print "\n"
 mano_der = mano_iz
 print mano_der, ", der"
 print mano_iz,  ", iz"
+print "\n"
 
+print "#### TERCERA PARTE PROCESO ####"
+print "\n"
 mano_iz = mano_extra
 print mano_iz, ", iz"
 print mano_extra , ", ex"
+print "\n"
+
+print "########    DESPUES"
+print "\n"
+print mano_der , ", der" 
+print mano_iz , ", iz"
+print "\n"
