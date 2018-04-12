719204a063f6f5b69dd58af46133edf92162606f
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-04-12 12:38:32 +0200

    Add files via upload
    Continuas Haciendo

---
+++ piedra_papel_tijeras.py
@@ -0,0 +1,100 @@
+# encoding: UTF-8
+# Nombre del programa ejercicio-bucle-promedio.py
+# hecho por: Daniel A Rodas Flores 
+
+# Procederé a importar os y time
+import time 
+import os
+
+# en desde la ĺínea 10 asta la 34 será mi portada del programa
+os.system("clear")
+print"""
+
+
+
+
+
+                            -------------------------
+                            |                       |
+                            |     Piedra, papel     |
+                            |                       |
+                            |        tijeras        |                             
+                            |                       |
+                            |       Programador:    |
+                            |                       |   
+                            |      Daniel Rodas     |
+                            |                       |
+                            -------------------------
+                            
+                            
+                            
+                            
+                            
+                           
+"""
+time.sleep(3)
+os.system("clear")
+
+print """
+
+
+            ############################################
+            #                                          # 
+            # Hola bienvenido a Piedra papel o tijeras #
+            #                                          #
+            ############################################
+            #                                          #
+            #          Pres. 0 para Salir              #
+            #                                          #                                      
+            #          Pres. 1 para Jugar              #
+            #                                          #
+            ############################################
+            
+            
+            
+            
+            """
+# Inicializaciones # Aqui van las variables
+salir = "N"
+turno_machine = 0
+entras_si_no= input("            Introduzca un número: ")
+
+if (entras_si_no == 0 ):
+    os.system("clear")
+    print """
+    
+    
+    
+    
+    
+    
+    
+    
+                                ############
+                                #          #   
+                                # Saliendo #
+                                #          #
+                                ############
+    
+ 
+ 
+ 
+ 
+            
+            """
+    time.sleep(3)
+    os.system("clear")
+    salir="S"
+else:
+	
+"""
+while ( salir=="N" ):
+    # Hago cosas
+    3...
+
+    # Incremento
+    2...
+    # Activo indicador de salida si toca
+    if ( 1... ): # Condición de salida
+            salir = "S"
+"""
