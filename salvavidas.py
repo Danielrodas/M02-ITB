f6ad57c5d2543aa6c7a86e5c0d690ed05283aab8
Daniel A. Rodas Flores <daniel.rodas123@gmail.com>
2018-04-27 13:01:46 +0200

    Add files via upload

---
+++ salvavidas.py
@@ -0,0 +1,42 @@
+# encoding: UTF-8
+# Nombre del programa:  salvavidas.py
+# hecho por: Daniel A Rodas Flores 
+
+# Procederé a importar os y time
+from time import sleep 
+from os import system
+from random import choice
+
+# en desde la ĺínea 10 asta la 34 será mi portada del programa
+system("clear")
+print"""
+                            -------------------------
+                            |                       |
+                            |      Salvavidas       |
+                            |                       |
+                            |       Minitánic       |
+                            |                       |
+                            |      Programador:     |
+                            |                       |
+                            |      Daniel Rodas     |
+                            |                       |
+                            -------------------------
+"""
+# Inicializaciones (Variables)
+salir = "N"
+Numeros_pasajeros_minitanic = 780
+Numeros_pasajeros_isla = 0
+Numeros_pasajeros_bote1= 0
+Numeros_vueltas = 0 
+
+
+while ( salir=="N" ):
+	# Hago cosas
+	Numeros_pasajeros_bote1 = 60
+	Numeros_pasajeros_minitanic = Numeros_pasajeros_minitanic - Numeros_pasajeros_bote1 
+
+	# Incremento
+	2...
+	# Activo indicador de salida si toca
+	if ( 1... ): # Condición de salida
+    	    salir = "S"
